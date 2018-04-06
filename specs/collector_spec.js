27905e7efc3ebafa931bdea08ec670c0fd18c7c6
Jim Bevington <jim.bevington@hotmail.com>
2018-04-06 16:04:18 +0100

    added Collector spec with pending tests

---
+++ specs/collector_spec.js
@@ -0,0 +1,45 @@
+const assert = require('assert');
+const Collector = require('../collector.js');
+const Record = require('../record.js');
+
+describe('Collector test', function(){
+
+  let collector1;
+  let collector2;
+  let record1;
+  let record2;
+  let record3;
+
+  beforeEach(function(){
+    collector1 = new Collector('Grayson', 150);
+    collector2 = new Collector('Grunhilda', 350);
+    record1 = new Record('Chamber Music', 'Iannis Xenakis', 12.99, 'Classical');
+    record2 = new Record('Returnal', 'Oneohtrix Point Never', 9.99, 'Electronic');
+    record3 = new Record('Elite Excel', 'Stellar Om Source', 11.88, 'Electronic');
+  })
+
+  it('can buy records');
+
+  it('can sell records');
+
+  it('buying decreases cash');
+
+  it('selling increases cash');
+
+  it('can\'t buy records that are too expensive');
+
+  it('can view collection value');
+
+  it('can view value by Genre');
+
+  it('can view valueS by Genre');
+
+  it('can print collection');
+
+  it('can sort records by Value - ascending');
+
+  it('can sort records by Value - descending');
+
+  it('can compare values with another collector');
+
+})
